8305310f0884314b657ab649021b130e542be6f4
adtak <adtak62@gmail.com>
2020-10-13 19:25:22 +0900

    feat: add arguments parser to batch job

--- src/batch_job.py
+++ src/batch_job.py
@@ -1,11 +1,21 @@
+import argparse
+
 from src.image_crawler import ImageCrawler
 from src.image_storage import ImageStorage
 
 
 def main():
+    args = parse_arguments()
     output_dir = "results"
-    results = ImageCrawler(output_dir).run("painting", 3)
+    results = ImageCrawler(output_dir).run(args.keyword, args.max)
     ImageStorage().push_images(output_dir, results)
+
+
+def parse_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-k", "--keyword", required=True)
+    arg_parser.add_argument("-m", "--max", required=True)
+    return arg_parser.parse_args()
 
 
 if __name__ == "__main__":
